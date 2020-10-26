69b79dae0a80b5a9b4b36f727d475fe703ece2bc
Deep112211 <72795614+Deep112211@users.noreply.github.com>
2020-10-26 19:51:33 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Bullet_Crash
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,51 @@
+var bullet, wall;
+var speed, weight, thickness;
+
+
+
+function setup() {
+  createCanvas(1600, 400);
+   
+  speed = random(223, 321);
+  weight = random(30, 52);
+  thickness = random(22, 83);
+
+  bullet = createSprite(50, 200, 100, 50);
+  bullet.velocityX = speed;
+  bullet.shapeColor = "yellow";
+  wall = createSprite(1200, 200, thickness, height / 2);
+
+}
+
+function draw() {
+  background(255,255,255);  
+
+  if (hasCollided(bullet, wall)) {
+    bullet.velocityX = 0;
+    var damage = 0.5 * weight * speed * speed / (thickness * thickness * thickness);
+
+    if (damage > 10) {
+      wall.shapeColor = "red";
+    }
+
+    if (damage < 10) {
+      wall.shapeColor = "green";
+    }
+  }
+
+  drawSprites();
+}
+
+
+function hasCollided(bullet, wall) {
+  bulletRightEdge = bullet.x + bullet.width;
+
+  wallLeftEdge = wall.x;
+
+  if (bulletRightEdge >= wallLeftEdge) {
+
+    return true;
+  }
+
+  return false;
+}
